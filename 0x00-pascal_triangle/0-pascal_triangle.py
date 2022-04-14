45dc51ae49ca53d08056d9157b11986f8e6af267
Capotedev <carolina.capote@imagineapps.co>
2022-04-14 13:39:46 -0500

    ADD - function that returns the pascal's triangle list of lists

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-Function that returns a list of lists of integers representing the Pascal’s
-triangle of n
+Pascals triangle method
 """
 
-def pascal_triangle(n):
-  pascal_triangle = []
-  if n > 0:
-    for i in range(n):  
-      pascal_triangle.append([])  
-      pascal_triangle[i].append(1)
-      for j in range(1, i):  
-        pascal_triangle[i].append(pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j])
-      
-      if i != 0:
-        pascal_triangle[i].append(1) 
 
-  return pascal_triangle
+def pascal_triangle(n):
+	"""
+	Function that returns a list of lists of integers representing the Pascals
+	triangle of n
+	"""
+	pascal_triangle = []
+	if n > 0:
+		for i in range(n):  
+			pascal_triangle.append([])
+			pascal_triangle[i].append(1)
+			for j in range(1, i):  
+				pascal_triangle[i].append(pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j])
+			if i != 0:
+				pascal_triangle[i].append(1) 
+
+	return pascal_triangle
