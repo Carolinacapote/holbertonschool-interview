a9c40103aaf14016dd6498f743a03b86efd0a669
Capotedev <carolina.capote@imagineapps.co>
2022-04-14 14:14:33 -0500

    DELETE whitespaces

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,12 +11,12 @@ def pascal_triangle(n):
     """
     new_list = []
     if n > 0:
-        for i in range(n):  
+        for i in range(n):
             new_list.append([])
             new_list[i].append(1)
-            for j in range(1, i):  
+            for j in range(1, i):
                 new_list[i].append(new_list[i - 1][j - 1] + new_list[i - 1][j])
             if i != 0:
-                new_list[i].append(1) 
+                new_list[i].append(1)
 
     return new_list
