bbdd45c023218ba2243c8cd929a3ddbb7f2f70d8
Capotedev <carolina.capote@imagineapps.co>
2022-04-26 12:43:44 -0500

    ADD - method to calculate the minimum operations to have nH in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste.
+"""
+
+
+def minOperations(n):
+    """
+    Given a number n, write a method that calculates the fewest number of
+    operations needed to result in exactly n H characters in the file.
+
+    Args:
+        n (int): number of H characters
+
+    Returns: int: minimum number of operations
+    """
+    if n > 1:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
+    return 0
