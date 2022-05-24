7a5fd88e3d1e9c10ad5e4d50b4f0fa7f746a76a4
Capotedev <carolina.capote@imagineapps.co>
2022-05-24 17:36:44 -0500

    FIX- conditionals

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -31,21 +31,15 @@ def print_chessboard(board, col, n):
 if __name__ == "__main__":
     import sys
 
-    try:
-        N = int(sys.argv[1])
-
-    except IndexError:
-        if len(sys.argv) != 2:
-            print("Usage: nqueens N")
-
-        else:
-            print("N must be a number")
-        sys.exit(1)
-
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(sys.argv[1])
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
